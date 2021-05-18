06afc12188bcab700dedef440f1ab65ba3be2568
yude <yudesleepy@gmail.com>
2021-05-18 20:17:49 +0900

    Update get-meter.sh

--- get-meter.sh
+++ get-meter.sh
@@ -3,4 +3,4 @@
 DEVICE_ID=
 TOKEN=
 
-/usr/bin/curl -X GET "https://api.switch-bot.com/v1.0/devices/${DEVICE_ID}/status" -H "Authorization: ${TOKEN}"
+/usr/bin/curl -X GET "https://api.switch-bot.com/v1.0/devices/${DEVICE_ID}/status" -H "Authorization: ${TOKEN}" | jq
